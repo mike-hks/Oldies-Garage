c3cde057cba091c88c5db9186680cabf4ff00429
Mykhailo Prezymirsky <LVIVSOFT\mprezymirsky@mprezymirsk.lvivsoft.local>
2016-03-10 18:46:19 +0200

    some parts of layout added

---
+++ client/helpers/helpers.js
@@ -0,0 +1,10 @@
+Template.introInfo.helpers ({
+	headingMain: "Welcome to Oldies Garage",
+	subheadingMain: "This is the right place for you to trade parts and get service for your classic cars"
+})
+
+Template.mainMenuImages.helpers({
+	title_1: "Trade Parts",
+	title_2: "Get Service",
+	title_3: "Articles"
+})
